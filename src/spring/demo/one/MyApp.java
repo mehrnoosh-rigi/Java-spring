85c824ad392a311ab95cd0417092e4cf6c0a3f05
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-09-15 09:56:17 +0200

    Add another class

--- src/spring/demo/one/MyApp.java
+++ src/spring/demo/one/MyApp.java
@@ -3,7 +3,7 @@ package spring.demo.one;
 public class MyApp {
     public static void main(String args[]){
         //create the object
-        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
         //use the object
         System.out.println(theCoach.getDailyWorkout());
     }
--- src/spring/demo/one/TrackCoach.java
+++ src/spring/demo/one/TrackCoach.java
@@ -1,2 +1,8 @@
-package spring.demo.one;public class TrackCoach {
+package spring.demo.one;
+
+public class TrackCoach implements Coach {
+    @Override
+    public String getDailyWorkout(){
+        return  "Run a hard 5 km";
+    }
 }
