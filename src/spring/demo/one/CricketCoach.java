6735ce65898360bfe81fb8dc07cccb6a9bf87178
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-09-15 18:08:37 +0200

    dependancy injection - literal values

--- src/spring/demo/one/CricketCoach.java
+++ src/spring/demo/one/CricketCoach.java
@@ -3,6 +3,9 @@ package spring.demo.one;
 public class CricketCoach implements Coach{
     private FortuneService fortuneService;
 
+    private String emailAddress;
+    private String team;
+
     public CricketCoach(){
         System.out.println("Inside no-arg CricketCoach");
     }
@@ -20,5 +23,24 @@ public class CricketCoach implements Coach{
     @Override
     public String getDailyFortune() {
         return "gerDailyFortune method in CricketCoach: " + fortuneService.getFortune();
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Inside setter method - setTeam");
+
+        this.team = team;
     }
 }
--- src/spring/demo/one/SetterDemoApp.java
+++ src/spring/demo/one/SetterDemoApp.java
@@ -12,6 +12,12 @@ public class SetterDemoApp {
         System.out.println(theCoach.getDailyWorkout());
 
         System.out.println(theCoach.getDailyFortune());
+
+        System.out.println(theCoach.getEmailAddress());
+
+        System.out.println(theCoach.getTeam());
+
+
         context.close();
     }
 }
